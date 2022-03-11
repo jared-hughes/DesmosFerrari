0c4f22b57591524ced0b388b3224dabfe0dc3946
Jared Hughes <jahughes241@gmail.com>
2022-03-11 00:30:11 -0800

    Fix unwanted wrapping at width

--- generate.js
+++ generate.js
@@ -9,13 +9,34 @@ setTimeout(async () => {
 
 const fs = require("fs");
 
+const MAX_VERTICES = 2000;
+
+function i_to_xy(i, width) {
+  return { x: i % width, y: Math.floor(i / width) };
+}
+
+function xy_to_i(xy, width) {
+  return xy.x + xy.y * width;
+}
+
+function add_xy(xy1, xy2) {
+  return { x: xy1.x + xy2.x, y: xy1.y + xy2.y };
+}
+
+function XY(x, y) {
+  return { x, y };
+}
+
 /**
  * Assume pixels is a 1D array in row-major order, representing a 2D array of
  * the given width.
  *
- * Return a 256-list of polygons. The polygons are disjoint, and their
+ * Return a 256-list of lists of polygons. The polygons are disjoint, and their
  * union is the full image. Each polygons is represented by a list of numbers,
- * where (number%width, number//width) is the corresponding vertex coordinate.
+ * where (number%(width+1), number//(width+1)) is the corresponding vertex
+ * coordinate.
+ *
+ * Adding one is required to properly handle vertices on the right edge
  */
 function getPolygons(pixels, width) {
   const polygonsByColor = Array.from({ length: 256 }).map((_) => []);
@@ -34,14 +55,31 @@ function getPolygons(pixels, width) {
       j++;
     }
     // Now, create a rectangle from i to j
-    polygonsByColor[pixels[i]].push([i, i + width, j + width, j]);
+    const start = i_to_xy(i, width);
+    const end = add_xy(i_to_xy(j - 1, width), XY(1, 0));
+    polygonsByColor[pixels[i]].push(
+      [start, add_xy(start, XY(0, 1)), add_xy(end, XY(0, 1)), end].map((xy) =>
+        xy_to_i(xy, width + 1)
+      )
+    );
   }
-  // Join up polygons of the same pixel color
+  // Join up polygons of the same pixel color, up to MAX_VERTICES elements
   const out = Array.from({ length: 256 }).map((_) => []);
   polygonsByColor.forEach((polygonList, i) => {
+    let currList = [];
     for (let polygon of polygonList) {
+      if (polygon.length + 2 >= MAX_VERTICES) {
+        throw `Polygon longer than ${MAX_VERTICES} vertices`;
+      }
+      if (polygon.length + currList.length + 2 >= MAX_VERTICES) {
+        out[i].push(currList);
+        currList = [];
+      }
       // join up with the first vertex, then add an undefined point
-      out[i].push(...polygon, polygon[0], "[][1]");
+      currList.push(...polygon, polygon[0], "[][1]");
+    }
+    if (currList.length > 0) {
+      out[i].push(currList);
     }
   });
   return out;
@@ -60,8 +98,8 @@ function polygonToLatex(polygon, width) {
     "polygon",
     "\\left[" +
       pointToLatex(
-        opnameCallToLatex("mod", "i", width),
-        opnameCallToLatex("floor", `\\frac{i}{${width}}`)
+        opnameCallToLatex("mod", "i", width + 1),
+        opnameCallToLatex("floor", `\\frac{i}{${width + 1}}`)
       ) +
       ` \\operatorname{for} i=\\left[${polygon}\\right]` +
       "\\right]"
@@ -72,8 +110,8 @@ CanvasCycle = {
   processImage: ({ width, height, colors, cycles, pixels }) => {
     polygons = getPolygons(pixels, width);
     // console.log("Dimensions", width, height);
-    // console.log("Polygon count:", polygons.length);
-    // console.log("Vertex count:", polygons.flat().length);
+    // console.log("Polygon count:", polygons.flat().length);
+    // console.log("Vertex count:", polygons.flat().flat().length);
     // console.log(
     //   "Unused palette colors:",
     //   polygons
@@ -81,7 +119,6 @@ CanvasCycle = {
     //     .filter(([p, _]) => p.length == 0)
     //     .map(([_, i]) => i)
     // );
-    // console.log("Total area:", polygons.map())
     const exprList = [
       {
         type: "folder",
@@ -89,14 +126,19 @@ CanvasCycle = {
         title: "Polygons",
         collapsed: true,
       },
-      ...polygons.slice(3, 4).map((polygon, index) => ({
-        type: "expression",
-        folderId: "ferrari-polygons",
-        colorLatex: `P_{${index}}`,
-        lines: false,
-        fillOpacity: "1",
-        latex: polygonToLatex(polygon, width),
-      })),
+      ...polygons
+        .map((polygonList, index) =>
+          polygonList.map((polygon, subindex) => ({
+            type: "expression",
+            folderId: "ferrari-polygons",
+            id: `ferrari-polygon-${index}-${subindex}`,
+            colorLatex: `P_{${index}}`,
+            lines: false,
+            fillOpacity: "1",
+            latex: polygonToLatex(polygon, width),
+          }))
+        )
+        .flat(),
     ];
     const state = {
       version: 9,
